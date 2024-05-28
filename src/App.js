5d97e3859252981d4bc60568a0602d9921b4f2f1
santiocampo1 <sanntiocampo@gmail.com>
2024-05-28 11:58:19 -0300

    Assets folder and creation of Nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Nav from './components/Nav'
 
 const App = () => {
   return (
-    <div className='App'>
-      App
-    </div>
+    <>
+      <Nav/>
+    </>
   )
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>This is the nav page</div>
+  )
+}
+
+export default Nav
