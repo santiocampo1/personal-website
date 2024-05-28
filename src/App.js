f3a150698271a213aaa7fe412697dbd8e04ec032
santiocampo1 <sanntiocampo@gmail.com>
2024-05-28 12:45:59 -0300

    Our initial components for our app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Nav from './components/Nav'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
     <Router>
       <Nav/>
       <Routes>
-        <Route path="/" element={<div>About</div>}/>
+        <Route path="/" element={About}/>
+        <Route path="/skills" element={Skills}/>
+        <Route path="/projects" element={Projects}/>
+        <Route path="/contact" element={Contact}/>
       </Routes>
     </Router>
   )
