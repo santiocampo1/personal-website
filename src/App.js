a6b6ba87708bfc5a776918162b8ca979c082fa80
santiocampo1 <sanntiocampo@gmail.com>
2024-05-29 10:25:36 -0300

    Styling the Nav component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import './styles/app.css'
 
 const App = () => {
   return (
