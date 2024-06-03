670aa8ea9e2927bcbf0fd8adb9190dfa89ffe148
santiocampo1 <sanntiocampo@gmail.com>
2024-06-03 09:37:13 -0300

    SkillsMenu component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Avatar from './Avatar';
+import SkillsMenu from './SkillsMenu';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <>
+      <Avatar page='skills'/>
+      <SkillsMenu />
+    </>
   )
 }
 
