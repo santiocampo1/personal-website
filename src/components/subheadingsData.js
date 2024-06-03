f8d6664be03430f509b0451fb0ab703cf73e4e8a
santiocampo1 <sanntiocampo@gmail.com>
2024-06-03 10:52:51 -0300

    About Page components [AboutMenu, AboutMenuItem, AboutSubHeading] and subHeadingsData file wich contains the data of the about menu.

---
+++ src/components/subheadingsData.js
@@ -0,0 +1,70 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                    Full Stack Developer with professional experience working on various projects, both collaboratively and individually. These have helped me to have a great adaptability to change, increase my agile learning, and be a totally self-taught person.
+                    </p>
+                </>
+            )
+        }
+    ],
+
+    2: [
+        {
+            title: "Full Stack Developer - Henry",
+            content: (
+                <>
+                    <p>
+                        Henry is an intensive 700-hour hands-on bootcamp that seeks to simulate a real work environment. It has allowed me to learn some of the most demanded technologies and tools, and to work in different projects under agile SCRUM methodology.
+                    </p>
+                </>
+            )
+        },
+
+        {
+            title: "Software Development Technician - IES Santa Fe",
+            content: (
+                <>
+                    <p>
+                        I am currently in my last year of my Technical Degree to solidify my knowledge in software development. I started it in 2022, and it allowed me to discover my passion in this profession.
+                    </p>
+                </>
+            )
+        },
+    ],
+
+    3: [
+        {
+            title: "Full Stack Developer - Taktiful Software Solutions",
+            content: (
+                <>
+                    <span>
+                        In Remote - (Orlando, Florida, USA).
+                    </span>
+                    <p>
+                        Taktiful is a leading consulting firm from USA, specializing in print beautification for brands, printers and OEMs. I had the opportunity to experience what it is like to work with a great IT team, which has made me grow a lot as a professional.
+                    </p>
+                </>
+            )
+        },
+
+        {
+            title: "Java Software Developer - Mindqube SAC",
+            content: (
+                <>
+                    <span>
+                        In Remote - (Lima, Perú).
+                    </span>
+                    <p>
+                        Experience as an intern, developing professionally as a Full Stack Developer. I had the opportunity to develop and manage a project for a client of the company. Working together with my technical and project leaders, from whom I learned several skills, we managed to meet the client's requirements within the stipulated deadline.
+                    </p>
+                </>
+            )
+        },
+    ],
+}
+
+export default subheadingsData;
